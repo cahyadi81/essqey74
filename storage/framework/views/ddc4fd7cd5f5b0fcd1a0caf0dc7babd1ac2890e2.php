86dc7aa532aee37424b174927bbe2b4c1fe4d60d
cahyadi81uff <cahyadi.pasaribu@unifam.com>
2025-12-11 13:40:47 +0700

    release storage folder

---
+++ storage/framework/views/ddc4fd7cd5f5b0fcd1a0caf0dc7babd1ac2890e2.php
@@ -0,0 +1,149 @@
+<?php $__env->startSection('title', 'PESAN HO'); ?>
+
+<?php $__env->startSection('content_header'); ?>
+  <div class="box-body">
+    <form id="send_pesan_ho" action="">
+    <div class="row">
+      <div class="col-md-12">
+      <div class="form-group">
+        <label>Option</label>
+          <select class="form-control select2" name="option" id="option" style="width: 100%;" tabindex="-1" aria-hidden="true">
+            <option value='ALL'>ALL</option>
+            <option value='NIK'>NIK</option>
+          </select>
+        </div>
+        <div class="form-group">
+          <label>Comp</label>
+          <select class="form-control select2" name="comp" id="comp" style="width: 100%;" tabindex="-1" aria-hidden="true">
+            <option value='addroo' selected="selected">ADDROO</option>
+            <option value='pma'>PINUS MERAH ABADI</option>
+            <option value='jalusi'>JALUSI</option>
+            <option value='sinar_anugrah'>SINAR ANUGRAH</option>
+            <option value='demo'>DEMO</option>
+          </select>
+        </div>
+        <div class="form-group">
+            <label>NIK</label>
+            <input type="text" disabled id="nik" name="nik" class="form-control" placeholder="NIK">
+          </div>
+      </div>
+      <!-- /.col -->
+      <div class="col-md-12 form-group">
+          <div class="form-group">
+              <label>HEADER</label>
+              <input type="text" id="header" name="header" class="form-control" placeholder="HEADER">
+          </div>
+          <textarea rows="10", cols="54" id="content" name="content" style="resize:none, "></textarea>
+      </div>
+      <div class="col-md-12 form-group">
+          <input type="submit" id="send" name="send" class="form-control btn btn-default" value="SEND" >
+      </div>
+      <!-- /.col -->
+    </div>
+    <!-- /.row -->
+    </form>
+  </div>
+<?php $__env->stopSection(); ?>
+
+
+
+<?php $__env->startSection('adminlte_js'); ?>
+  <script>
+    $(document).ready(function(){
+        $('.select2').select2();
+        /* Attach a submit handler to the form */
+        $(document).on("submit", "#send_pesan_ho", function(event) {
+          
+          var ajaxRequest;
+
+          var isDisabled = $('#nik').prop('disabled');
+
+          $('#send').attr('disabled','disabled');
+          $("#send").attr('value','Sending...');
+          
+          /* Stop form from submitting normally */
+          event.preventDefault();
+
+          /* Clear result div*/
+          $("#result").html('');
+
+          /* Get from elements values */
+          var values = $(this).serialize();
+          var target = "";
+          var option = $("#option :selected").val();
+          var comp = $("#comp :selected").val();
+          var nik = $("#nik").val();
+          if(isDisabled){
+            target = option;
+          }else{
+            target = nik;
+          }
+
+          /* Send the data using post and put the results in a div */
+          /* I am not aborting previous request because It's an asynchronous request, meaning 
+            Once it's sent it's out there. but in case you want to abort it  you can do it by  
+            abort(). jQuery Ajax methods return an XMLHttpRequest object, so you can just use abort(). */
+            ajaxRequest= $.ajax({
+                url: 'http://phpstack-160315-572402.cloudwaysapps.com/api/'+comp+'/pesan_ho/send?target='+target,
+                type: "post",
+                data: values
+            });
+
+          /*  request cab be abort by ajaxRequest.abort() */
+
+          ajaxRequest.done(function (response, textStatus, jqXHR){
+              // show successfully for submit message
+              $('#send').removeAttr('disabled');
+              $("#send").attr('value','Send');
+              alert('Send Pesan successfully');
+              location.reload();
+              
+
+          });
+
+            /* On failure of request this function will be called  */
+          ajaxRequest.fail(function (){
+
+            // show error
+            $('#send').removeAttr('disabled');
+            $("#send").attr('value','Send');
+            alert('There is error while submit');
+            
+          });
+          return false;
+        });
+     });
+
+
+
+     $(function() { $('#content').froalaEditor({
+        codeBeautifierOptions: {
+          end_with_newline: true,
+          indent_inner_html: true,
+          extra_liners: "['p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'blockquote', 'pre', 'ul', 'ol', 'table', 'dl']",
+          brace_style: 'expand',
+          indent_char: ' ',
+          indent_size: 4,
+          wrap_line_length: 0
+        },
+        height:180,
+      }) 
+    })
+    
+    $("#option").change(function(){
+      val = $("#option :selected").val();
+      if(val == "ALL"){
+        $("#nik").prop( "disabled", true );
+      }else if(val == "NIK"){
+        $("#nik").prop( "disabled", false );
+      }
+      
+    })
+
+  </script>
+  <script src="<?php echo e(asset('vendor/adminlte/dist/js/adminlte.min.js')); ?>"></script>
+
+    <?php echo $__env->yieldPushContent('js'); ?>
+    <?php echo $__env->yieldContent('js'); ?>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('adminlte::page', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
---
+++ storage/framework/views/f40adaaf9531c715213ce53298cf3e91813a286b.php
@@ -0,0 +1,148 @@
+<?php 
+$target = $_REQUEST['target'] == null ? '' : $_REQUEST['target']; 
+function convert($target){
+  if($target == 'sinar_anugrah'){
+    return "SINAR ANUGRAH";
+  }else if($target == 'demo'){
+    return "DEMO QEY";
+  }else if($target == 'pma'){
+    return "PINUS MERAH ABADI";
+  }else if($target == 'jalusi'){
+    return "JALUSI";
+  }else if($target == 'DEMO'){
+    return "DEMO";
+  }else if($target == 'SELULAR'){
+    return "SELULAR";
+  }
+  else{
+    return "";
+  }
+}
+?>
+<?php $__env->startSection('title', 'ESS - User Monitoring'); ?>
+
+<?php $__env->startSection('content_header'); ?>
+    <h1>User Monitoring - <?php echo convert($target); ?></h1>
+<?php $__env->stopSection(); ?>
+
+<?php $__env->startSection('content'); ?>
+
+
+
+  <table class="table table-bordered table-hover" id="table">
+      <thead>
+        <tr>
+            <th>NO</th>
+            <th>CODE USER</th>
+            <th>NAME USER</th>
+            <th>USERNAME</th>
+            <th>JOB</th>
+            <th>STATUS</th>
+            <th>LAST LOGIN</th>
+            <th></th>
+        </tr>
+      </thead>
+  </table>
+<?php $__env->stopSection(); ?>
+
+<?php $__env->startSection('adminlte_js'); ?>
+  <script>
+    var no = 1;  
+    var jml_logout = 0;
+    var jml_login = 0;
+
+    
+	var host = "";
+    
+    $dtbl = $('#table').DataTable({
+       "processing": true,
+       "serverSide": true,
+       
+       ajax: {
+         
+         url: host+"/api/<?php echo e($target); ?>/null/login/data/user?pass=$2y$12$pEtbFeVYpdXlP9CmVjffhuEk9WDM0pCbTjmVnWgO9qgQo.U0OtsX2",
+         type: "GET",
+         crossDomain: true,
+         headers: {
+          "accept": "application/json",
+          "Access-Control-Allow-Origin":"*"
+         },
+       },
+       order: [['1', "desc"]],
+       columnDefs: [{ "orderable": false, "searchable": false, "targets": 0 }],
+       columns: [
+                {
+                  "data": "kd_user",
+                  render: function (data, type, row, meta) {
+                      return meta.row + meta.settings._iDisplayStart + 1;
+                  },
+                  "orderable": "false"
+               },
+               { data: 'kd_user', name: 'kd_user' },
+               { data: 'nm_user', name: 'nm_user' },
+               { data: 'username', name: 'username' },
+               {
+                "render": function (data, type, JsonResultRow, meta) {
+
+
+                  if(JsonResultRow.nm_jabatan == '' || JsonResultRow.nm_jabatan == null){  
+                    return "--NOT HAVE JOB--";
+                  }else{
+                    return JsonResultRow.nm_jabatan;
+                  }
+                }
+               },
+               {
+                 "render": function (data, type, JsonResultRow, meta) {
+                  
+
+
+                   if(JsonResultRow.is_login == '1'){  
+                     return "<i class='fas fa-lock-open' style='color:green;'> Login</i>";
+                   }else{return "<i class='fas fa-lock' style='color:red;'> Logout</i>";
+                   }
+                 }
+               },
+               { data: 'last_login', name: 'last_login' },
+               {
+                "render": function (data, type, JsonResultRow, meta) {
+                  if(JsonResultRow.is_login == '1'){  
+                    return "<button onClick=force_logout('"+JsonResultRow.username+"') class='btn btn-info'><i class='glyphicon glyphicon-log-in'> FORCE LOGOUT</i></button>";
+                  }else{
+                    return '';
+                  }
+                }
+              },
+             ]
+       ,select: true,
+    });
+
+
+    function force_logout(username){
+      console.log("logout....");
+      $.ajax({
+        type: 'POST',
+        url: host+"/api/<?php echo e($target); ?>/null/login/data/user/logout?username="+username,
+        
+        //OR
+        //beforeSend: function(xhr) { 
+        //  xhr.setRequestHeader("My-First-Header", "first value"); 
+        //  xhr.setRequestHeader("My-Second-Header", "second value"); 
+        //}
+      }).done(function(data,d,xhr) {   
+        $dtbl.ajax.reload();
+      });
+    }
+
+    
+    var versionNo = $.fn.dataTable.version;
+    
+  </script>
+    <script src="<?php echo e(asset('vendor/adminlte/dist/js/adminlte.min.js')); ?>"></script>
+    
+  
+    
+    <?php echo $__env->yieldPushContent('js'); ?>
+    <?php echo $__env->yieldContent('js'); ?>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('adminlte::page', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
